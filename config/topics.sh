9f1f3c0bd9e63cf352ae245b861adce479598f9a
chanyoungjung <hy910915@kaist.ac.kr>
2022-06-26 10:44:20 -0700

    Housekeeping

---
+++ config/topics.sh
@@ -0,0 +1,28 @@
+rosbag record \
+/os_cloud_node/points \
+/scout/mavros/imu/data \
+/BLIO/odom_gicp_global_prediction \
+/BLIO/Globalmap \
+/BLIO/Localmap \
+/BLIO/Keyframe \
+/BLIO/KNN_Keyframe \
+/BLIO/KNN_Keyframe_map \
+/BLIO/Keyframe_map_align \
+/scout/mavros/vision_pose/pose \
+/scout/mavros/local_position/pose \
+/scout/mavros/setpoint_velocity/cmd_vel \
+/scout/mavros/local_position/velocity_local \
+/scout/global_goal \
+/scout/local_goal \
+/scout/map \
+/scout/map_distance \
+/scout/map_check \
+/tf \
+/usb_cam/image_raw/compressed \
+/scout/astar_path_vis \
+/scout/marker_target \
+/scout/astar_path \
+/GoalAction \
+/GoalPoint \
+/Mission \
+/scout/mavros/state
